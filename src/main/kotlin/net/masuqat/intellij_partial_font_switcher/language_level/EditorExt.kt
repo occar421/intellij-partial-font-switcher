c1ac994557deef2a0a5e4dd6347bf88f1ac7a6f6
MasuqaT <occar@hotmail.co.jp>
2025-10-12 20:16:54 +0900

    Switch font with fixed ones

--- src/main/kotlin/net/masuqat/intellij_partial_font_switcher/language_level/EditorExt.kt
+++ src/main/kotlin/net/masuqat/intellij_partial_font_switcher/language_level/EditorExt.kt
@@ -1,15 +1,15 @@
 package net.masuqat.intellij_partial_font_switcher.language_level
 
-import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.Editor
 
 val fontSelector = LanguageFontSelector()
 
 // TODO 適切なフォントの指定を考える
 
-fun Editor.overrideWithLanguageFont(): Unit {
+fun Editor.overrideWithLanguageFont() {
     this.colorsScheme.editorFontName = fontSelector.select(this.virtualFile.fileType)
 }
 
-fun Editor.revertLanguageFont(): Unit {
+fun Editor.revertLanguageFont() {
     this.colorsScheme.editorFontName = fontSelector.defaultFontName
 }
