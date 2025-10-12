5b958138271c240498ea2af8499c6c8b7b53c365
MasuqaT <occar@hotmail.co.jp>
2025-10-12 20:49:20 +0900

    Add config page

---
+++ src/main/kotlin/net/masuqat/intellij_partial_font_switcher/AppConfig.kt
@@ -0,0 +1,24 @@
+package net.masuqat.intellij_partial_font_switcher
+
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.util.NlsContexts
+import javax.swing.JComponent
+import javax.swing.JPanel
+
+class AppConfig : Configurable, Configurable.Beta {
+    override fun getDisplayName(): @NlsContexts.ConfigurableName String {
+        return "Partial Font Switcher"; // TODO: from resource
+    }
+
+    override fun createComponent(): JComponent {
+        return JPanel() // TODO: impl
+    }
+
+    override fun isModified(): Boolean {
+        return false // TODO: impl
+    }
+
+    override fun apply() {
+        // TODO: impl
+    }
+}
