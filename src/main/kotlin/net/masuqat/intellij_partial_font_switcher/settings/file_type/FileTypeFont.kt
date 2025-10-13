4fbd835851e72d89b2bb4d924b1f64db6fe7637a
MasuqaT <occar@hotmail.co.jp>
2025-10-13 16:12:24 +0900

    Show FileType combobox

--- src/main/kotlin/net/masuqat/intellij_partial_font_switcher/settings/file_type/FileTypeFont.kt
+++ src/main/kotlin/net/masuqat/intellij_partial_font_switcher/settings/file_type/FileTypeFont.kt
@@ -3,23 +3,26 @@ package net.masuqat.intellij_partial_font_switcher.settings.file_type
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.options.UnnamedConfigurable
+import com.intellij.openapi.ui.ComboBoxTableRenderer
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.ui.ToolbarDecorator
-import com.intellij.ui.table.JBTable
+import com.intellij.ui.table.TableView
 import com.intellij.util.ui.ColumnInfo
 import com.intellij.util.ui.ListTableModel
 import net.masuqat.intellij_partial_font_switcher.services.AppSettings
 import net.masuqat.intellij_partial_font_switcher.services.FileTypeFont
+import javax.swing.Icon
 import javax.swing.JComponent
 
 class FileTypeFontTable : UnnamedConfigurable {
     class Model :
         ListTableModel<Model.FileTypeFont>(FileTypeColumnInfo(), FontColumnInfo()) {
-        data class FileTypeFont(val fileType: FileType, val font: String)
+        data class FileTypeFont(var fileType: FileType, var font: String)
     }
 
     val model = Model()
 
-    val table = JBTable(model).apply {
+    val table = TableView(model).apply {
         emptyText.text = "No FileType font settings" // TODO from resource
         visibleRowCount = 5
     }
@@ -27,11 +30,7 @@ class FileTypeFontTable : UnnamedConfigurable {
     override fun createComponent(): JComponent {
         return ToolbarDecorator.createDecorator(table)
             .setAddAction {
-                val fileTypes = FileTypeManager.getInstance().registeredFileTypes.toSet()
-                val configuredFileTypes = model.items.map { it.fileType }.toSet()
-                val availableFileTypes = fileTypes - configuredFileTypes
-
-                model.addRow(Model.FileTypeFont(availableFileTypes.first(), "Impact2"))
+                model.addRow(Model.FileTypeFont(fileTypes().first(), "Impact2"))
             }
             .setRemoveAction {
                 model.removeRow(table.selectedRow)
@@ -61,9 +60,38 @@ class FileTypeFontTable : UnnamedConfigurable {
 }
 
 class FileTypeColumnInfo : ColumnInfo<FileTypeFontTable.Model.FileTypeFont, String>("FileType") { // TODO from resource
-    override fun valueOf(lf: FileTypeFontTable.Model.FileTypeFont?): String? = lf?.fileType?.displayName
+    private val fileTypeMap = fileTypes().associateBy { type -> type.name }
+
+    override fun valueOf(lf: FileTypeFontTable.Model.FileTypeFont?): String? = lf?.fileType?.name
+
+    override fun setValue(item: FileTypeFontTable.Model.FileTypeFont?, value: String?) {
+        item?.fileType = fileTypeMap[value] ?: return
+    }
+
+    override fun getEditor(item: FileTypeFontTable.Model.FileTypeFont?) = createComboBoxRendererAndEditor()
+
+    override fun getRenderer(item: FileTypeFontTable.Model.FileTypeFont?) = createComboBoxRendererAndEditor()
+
+    override fun isCellEditable(item: FileTypeFontTable.Model.FileTypeFont?) = true
+
+    private fun createComboBoxRendererAndEditor(): ComboBoxTableRenderer<String> {
+        val fileTypes = fileTypes().map { it.name }
+
+        return FileTypeCellComboBox(fileTypes.toTypedArray()).withClickCount(1)
+    }
+
+    private class FileTypeCellComboBox(values: Array<String>) : ComboBoxTableRenderer<String>(values) {
+        private val fileTypeMap = fileTypes().associateBy { type -> type.name }
+
+        override fun getTextFor(value: String): @NlsContexts.Label String =
+            fileTypeMap[value]?.displayName ?: "???" // TODO from resource
+
+        override fun getIconFor(value: String): Icon? = fileTypeMap[value]?.icon
+    }
 }
 
 class FontColumnInfo : ColumnInfo<FileTypeFontTable.Model.FileTypeFont, String>("Font") { // TODO from resource
     override fun valueOf(lf: FileTypeFontTable.Model.FileTypeFont?): String? = lf?.font
 }
+
+private fun fileTypes() = FileTypeManager.getInstance().registeredFileTypes
